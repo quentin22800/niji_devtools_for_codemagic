1fc5a989869a554b16e2478a9a7b79b4e6d67c66
Quentin EONO <quentin.eono@niji.fr>
2021-11-19 14:41:20 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/niji_devtools_for_codemagic/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.niji_devtools_for_codemagic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ push.sh
@@ -0,0 +1,7 @@
+echo "# niji_devtools_for_codemagic" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/quentin22800/niji_devtools_for_codemagic.git
+git push -u origin main
